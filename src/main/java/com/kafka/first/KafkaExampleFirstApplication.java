27b2e529c19936e79b343b631bcea5b426b8e257
alex <forn.sky@yandex.ru>
2021-11-06 21:39:09 +0300

    +

---
+++ src/main/java/com/kafka/first/KafkaExampleFirstApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaExampleFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaExampleFirstApplication.class, args);
+    }
+
+}
